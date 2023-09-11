4485ee34d5f963285c5af01441160a429dd31114
learuis <144658716+learuis@users.noreply.github.com>
2023-09-11 10:40:14 -0400

    Add files via upload

---
+++ common.py
@@ -0,0 +1,72 @@
+import discord
+import re
+import time
+from discord import ui
+from discord.ext import commands
+import sqlite3
+from datetime import date
+
+def custom_cooldown(ctx):
+    whitelist = {'Admin', 'Moderator'}
+    roles = {role.name for role in ctx.author.roles}
+    if not whitelist.isdisjoint(roles):
+        #if we're a special role, no cooldown assigned
+        return None
+    else:
+        #everyone else
+        return discord.app_commands.Cooldown(3, 60)
+
+def checkChannel(ctx):
+    execTime = time.strftime('%c')
+    print(f'Command {ctx.command} executed by {ctx.author} on {execTime}')
+    return ctx.channel.id == 1144882044552364093
+
+def isInt(intToCheck):
+    try:
+        int(intToCheck)
+    except ValueError:
+        return False
+    else:
+        return True
+
+def ununicode(string):
+    output = re.sub(r'[^\x00-\x7F]', '?', string)
+    output = output.replace('??', '?')
+    return output
+
+def percentage(int1, int2):
+    ratio = (int1 / int2) * 100
+    ratio = round(ratio)
+    return ratio
+
+class RegistrationButton(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Register your Character", style=discord.ButtonStyle.green, custom_id="my_custom_button")
+    async def register_character(self, interaction: discord.Interaction, button: discord.ui.button):
+        await interaction.response.send_modal(RegistrationForm())
+
+class RegistrationForm(ui.Modal, title='Band of Outcasts Character Registration'):
+    charName = ui.TextInput(label='Character Name')
+    funcomId = ui.TextInput(label='Funcom ID')
+
+    async def on_submit(self, interaction: discord.Interaction):
+
+        con_sub = sqlite3.connect(f'data/VeramaBot.db'.encode('utf-8'))
+        cur_sub = con_sub.cursor()
+        cur_sub.execute(f'insert into registration '
+                        f'(discord_user,character_name,funcom_id,registration_date,season) values '
+                        f'(\'{interaction.user}\',\'{self.charName}\',\'{self.funcomId}\','
+                        f'\'{date.today()}\',3)')
+        con_sub.commit()
+        con_sub.close()
+
+        await interaction.response.send_message(f'Registered character: {self.charName} '
+                                                f'with Funcom ID: {self.funcomId} '
+                                                f'to user {interaction.user.mention}', ephemeral=True)
+
+        channel = interaction.client.get_channel(1150628473061253251)
+        await channel.send(f'__Character Name:__ {self.charName}\n'
+                           f'__Funcom ID:__ {self.funcomId}\n'
+                           f'__Discord:__ {interaction.user.mention}')
---
+++ externalConnections.py
@@ -0,0 +1,88 @@
+import re
+import os
+from rcon import Console
+from rcon.util import remove_formatting_codes
+from ftplib import FTP
+
+from dotenv import load_dotenv
+
+load_dotenv('bandofoutcasts_env.txt')
+RCON_HOST = os.getenv('RCON_HOST')
+RCON_PORT = os.getenv('RCON_PORT')
+RCON_PASS = os.getenv('RCON_PASS')
+FTP_HOST = os.getenv('FTP_HOST')
+FTP_PORT = os.getenv('FTP_PORT')
+FTP_USER = os.getenv('FTP_USER')
+FTP_PASS = os.getenv('FTP_PASS')
+
+def downloadSave():
+
+    ftp = FTP()
+
+    #os.chdir('data')
+
+    ftp.connect(FTP_HOST, int(FTP_PORT))
+    ftp.login(FTP_USER, FTP_PASS)
+    ftp.cwd(r'ConanSandbox\saved\Logs')
+    ftp.encoding = 'latin-1'
+
+    # For text files
+
+    lines = []
+    ftp.retrlines('RETR ConanSandbox.log', lambda d: lines.append(d + '\n'))
+    f = open('ConanSandbox.log', 'w')
+    f.writelines(lines)
+    returnFile = f
+    f.close()
+
+    ftp.close()
+
+    return returnFile
+
+def runRcon(command: str):
+
+    class RconResponse:
+        def __init__(self):
+            self.output = []
+            self.error = 0
+
+    returnValue = RconResponse()
+    
+    commandOutput = []
+    failures = 0
+    
+    while failures < 6:
+        try:
+            console = Console(host=RCON_HOST, port=int(RCON_PORT), password=RCON_PASS)
+            break
+        except Exception:
+            failures += 1
+
+    if failures == 6:
+        returnValue.output = ['Authentication failed 5 times in a row.']
+        returnValue.error = 1
+        return returnValue
+    
+    #add error handling here
+    res_body = console.command(command).body
+    console.close()
+
+    res_body = remove_formatting_codes(res_body)
+
+    if not res_body.endswith('\n'):
+        res_body += '\n'
+
+    res_list = res_body.splitlines()
+    
+    for x in res_list:
+        if command == 'listplayers':
+            x = re.sub(r'\s', '', x)
+            x = re.sub(r'\|', ',', x)
+            commandOutput.append(x.split(','))
+        else:
+            commandOutput.append(x)
+
+    returnValue.output = commandOutput
+    return returnValue
+
+    #return commandOutput
