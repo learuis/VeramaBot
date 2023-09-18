9ca008476fd8efa2ba134892beb6dfe0f7bf967e
learuis <rmplear@gmail.com>
2023-09-18 14:01:32 -0400

    beginning work on god selection

--- cogs/NameChange.py
+++ cogs/NameChange.py
@@ -1,8 +1,27 @@
+import discord
 from discord.ext import commands
+from functions.common import custom_cooldown, checkChannel
 
 class NameChange(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+    @commands.command(name='namechange', aliases=['changename', 'setname'])
+    @commands.has_any_role('Admin', 'Moderator')
+    @commands.dynamic_cooldown(custom_cooldown, type=commands.BucketType.user)
+    @commands.check(checkChannel)
+    async def changeName(self, ctx, discord_user: discord.Member):
+        """
+
+        Parameters
+        ----------
+        ctx
+        discord_user
+
+        Returns
+        -------
+
+        """
 
 @commands.Cog.listener()
 async def setup(bot):
--- functions/common.py
+++ functions/common.py
@@ -187,3 +187,26 @@ class RegistrationForm(ui.Modal, title='Character Registration'):
         await channel.send(f'__Character Name:__ {self.charName}\n'
                            f'__Funcom ID:__ {self.funcomId}\n'
                            f'__Discord:__ {interaction.user.mention}')
+
+    class GodSelection(discord.ui.Select):
+        def __init__(self):
+            options = [
+                discord.SelectOption(label="Derketo", emoji="�", description="This is option 1!"),
+                discord.SelectOption(label="Option 2", emoji="✨", description="This is option 2!"),
+                discord.SelectOption(label="Option 3", emoji="🎭", description="This is option 3!")
+            ]
+            super().__init__(placeholder="Select an option", max_values=1, min_values=1, options=options)
+
+        async def callback(self, interaction: discord.Interaction):
+            if self.values[0] == "Option 1":
+                await interaction.response.edit_message(content="This is the first option from the entire list!")
+            elif self.values[0] == "Option 2":
+                await interaction.response.send_message("This is the second option from the list entire wooo!",
+                                                        ephemeral=False)
+            elif self.values[0] == "Option 3":
+                await interaction.response.send_message("Third One!", ephemeral=True)
+
+    class SelectView(discord.ui.View):
+        def __init__(self, *, timeout=180):
+            super().__init__(timeout=timeout)
+            self.add_item(Select())
