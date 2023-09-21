a3104f4fd062cc30a14ddcf1a7b50757cd0d805a
learuis <rmplear@gmail.com>
2023-09-21 14:18:27 -0400

    added max length to reg modal

--- cogs/Registration.py
+++ cogs/Registration.py
@@ -28,8 +28,10 @@ class RegistrationButton(discord.ui.View):
 
 # noinspection PyUnresolvedReferences
 class RegistrationForm(ui.Modal, title='Character Registration'):
-    charName = ui.TextInput(label=f'Character Name', placeholder='Type your full, exact in-game character name')
-    funcomId = ui.TextInput(label=f'Funcom ID', placeholder='Find this in game by pressing L')
+    charName = ui.TextInput(label=f'Character Name', placeholder='Type your full, exact in-game character name',
+                            max_length=30)
+    funcomId = ui.TextInput(label=f'Funcom ID', placeholder='Find this in game by pressing L',
+                            max_length=30)
 
     async def on_submit(self, interaction: discord.Interaction):
 
